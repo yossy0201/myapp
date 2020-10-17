fd12aca2fa9700e90a399fa046df30e9e9ad9ab9
yossy0201 <yossy0201msk@gmail.com>
2020-10-18 00:33:43 +0900

    display

---
+++ routes/views/action.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+  <title>My Book App</title>
+</head>
+<body>
+<h1>あなたの読んだ本</h1>
+
+こんにちは、<?php echo $_POST['name'] ?>さん
+<br>
+あなたが本を読み終えた日付は、<?php echo $_POST['date'] ?>です。
+<br>
+あなたの読んだ本は、<?php echo $_POST['book-name'] ?>です。
+<br>
+あなたの本の感想は、<?php echo $_POST['book-comment'] ?>です。
+<br>
+
+</body>
+</html>
--- routes/views/books-memory.php
+++ routes/views/books-memory.php
@@ -6,8 +6,9 @@
 <h1>ブック管理ページへようこそ</h1>
 
 <form action="action.php" method="post">
-  本の名前: <input type="text" name="name" />
-  読んだ日付: <input type="text" name="book-name" />
+  名前: <input type="text" name="name" />
+  本を読んだ日: <input type="text" name="date" />
+  本の名前: <input type="text" name="book-name" />
   本の感想: <input type="text" name="book-comment" />
   <input type="submit">
 </form>

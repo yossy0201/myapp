924123b925e1dcb9023683a58213389096527c2b
yossy0201 <yossy0201msk@gmail.com>
2020-10-18 00:20:13 +0900

    book

---
+++ routes/views/books-memory.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+  <title>My Book App</title>
+</head>
+<body>
+<h1>ブック管理ページへようこそ</h1>
+
+<form action="action.php" method="post">
+  本の名前: <input type="text" name="name" />
+  読んだ日付: <input type="text" name="book-name" />
+  本の感想: <input type="text" name="book-comment" />
+  <input type="submit">
+</form>
+
+</body>
+</html>

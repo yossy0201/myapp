89a9a2d14ad752e2c4f872ad0600b96e97e4fedc
yossy0201 <yossy0201msk@gmail.com>
2020-10-21 23:46:58 +0900

    alert

---
+++ routes/views/hello.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <title>Document</title>
+  <link rel="stylesheet" href ="style.css">
+
+  <script>
+  alert("こんにちは");
+  </script>
+
+</head>
+<body>
+  
+</body>
+</html>
